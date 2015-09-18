797166d2e26161cfb74316bc9207ba2b32dfc64c
dormesica <dormesica10@gmail.com>
2015-09-18 10:54:02 +0300

    delete things from app.js

--- app.js
+++ app.js
@@ -140,7 +140,3 @@ var generateDictionary = function (json, dict) {
 module.exports = {
 	jsonToXml: jsonToXml
 };
-
-var jsonString = '{"glossary": {"title": "example glossary","GlossDiv": {"title": "S","GlossList": {"GlossEntry": {"ID": "SGML","SortAs": "SGML","GlossTerm": "Standard Generalized Markup Language","Acronym": "SGML","Abbrev": "ISO 8879:1986","GlossDef": {"para": "A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso": ["GML", "XML"]},"GlossSee": "markup"}}}}}';
-var xml = jsonToXml(JSON.parse(jsonString));
-console.log(xml.toString({ pretty: true }));
